d89582e23642a1df573131c035fa831ff53321e7
jamesslone <jamestslone@gmail.com>
2025-05-10 10:01:12 -0400

    creates fashion-models.html content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('mainNav');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-shrink');
+    } else {
+        navbar.classList.remove('navbar-shrink');
+    }
+});
+
