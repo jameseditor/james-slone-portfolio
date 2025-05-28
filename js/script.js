9e730a1a8ef6ba971c3c13762d6d085968470547
jamesslone <jamestslone@gmail.com>
2025-05-28 11:53:33 -0400

    tested video player pop up on achive.html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const modal = document.getElementById('videoModal');
-    const iframe = document.getElementById('videoFrame');
 
-    modal.addEventListener('hidden.bs.modal', () => {
-        iframe.src = iframe.src; // Reset video
+
+
+const videoFrame = document.getElementById('videoFrame');
+const videoModal = document.getElementById('videoModal');
+let currentVideoURL = "";
+
+document.querySelectorAll('.video-thumbnail').forEach(thumbnail => {
+    thumbnail.addEventListener('click', function () {
+        const baseURL = this.getAttribute('data-video');
+        currentVideoURL = baseURL;
+        videoFrame.src = baseURL + "&autoplay=1";
     });
 });
+
+videoModal.addEventListener('hidden.bs.modal', () => {
+    videoFrame.src = ""; // Clear to stop the video
+});
+
